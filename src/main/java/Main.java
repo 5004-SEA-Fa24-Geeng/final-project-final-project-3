439d9121c9c5b923da0add157f77e5de36b0d2ae
Alvin <jamesxsj8441@126.com>
2025-04-18 14:51:30 -0700

    add comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 import controller.MainController;
 
+/**
+ * Main class to start the application
+ * @author Yuchen Huang
+ * @version 1.0
+ * @since 2025-04-14
+ */
 public class Main {
     public static void main(String[] args) {
         // use SwingUtilities to ensure running on the event dispatch thread
